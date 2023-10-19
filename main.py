84fb58a4a9a40910e4786e1533ed6e0c28ebf70e
Dhruv1401 <85736475+Dhruv1401@users.noreply.github.com>
2023-10-19 17:45:35 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+
+def guess_the_number():
+    # Generate a random number between 1 and 100
+    secret_number = random.randint(1, 100)
+    
+    attempts = 0
+    max_attempts = 10
+    
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    print(f"You have {max_attempts} attempts to guess the number.")
+    
+    while attempts < max_attempts:
+        try:
+            # Get the player's guess
+            guess = int(input("Enter your guess: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+        
+        attempts += 1
+        
+        if guess < secret_number:
+            print("Too low! Try again.")
+        elif guess > secret_number:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You guessed the number ({secret_number}) in {attempts} attempts.")
+            break
+    
+    if attempts >= max_attempts:
+        print(f"Sorry, you've reached the maximum number of attempts. The secret number was {secret_number}.")
+
+if __name__ == "__main__":
+    guess_the_number()
